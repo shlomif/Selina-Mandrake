17cbe325bd3838d734c3e3652416071107bdcf52
Shlomi Fish <shlomif@shlomifish.org>
2014-08-24 14:34:52 +0300

    Remove unused vars

--- selina-mandrake/screenplay/scripts/prepare-epub.pl
+++ selina-mandrake/screenplay/scripts/prepare-epub.pl
@@ -19,8 +19,6 @@ my $obj = Shlomif::Screenplays::EPUB->new(
 $obj->run;
 
 my $gfx = $obj->gfx;
-my $out_fn = $obj->out_fn;
-my $target_dir = $obj->target_dir;
 
 {
     my $epub_basename = 'selina-mandrake';
