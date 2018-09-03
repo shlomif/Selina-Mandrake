b6ee8038fdb7c6c0189ae8505199cd0d2e7271fd
Shlomi Fish <shlomif@shlomifish.org>
2018-09-03 15:27:00 +0300

    Convert to CI-gen.
    See https://github.com/shlomif/perl6-CI-Gen .

--- .travis.bash
+++ .travis.bash
@@ -9,7 +9,21 @@ set -e
 set -x
 cmd="$1"
 shift
-if test "$cmd" = "build"
+if false
+then
+    :
+elif test "$cmd" = "before_install"
+    sudo apt-get update -qq
+    sudo apt-get install -y ack-grep cpanminus docbook-defguide docbook-xsl libperl-dev libxml-libxml-perl libxml-libxslt-perl make perl tidy xsltproc
+    sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
+    cpanm local::lib
+then
+elif test "$cmd" = "install"
+then
+    cpanm --notest Alien::Tidyp YAML::XS
+    bash -x bin/install-tidyp-systemwide.bash
+    cpanm --notest HTML::Tidy
+elif test "$cmd" = "build"
 then
     export SCREENPLAY_COMMON_INC_DIR="$PWD/screenplays-common"
     cd selina-mandrake/screenplay/
